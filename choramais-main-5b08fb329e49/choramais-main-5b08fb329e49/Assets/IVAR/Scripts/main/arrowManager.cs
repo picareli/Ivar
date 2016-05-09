12d2bb67d3ef34673245df16f70ad9153845b47c
Eric Spenner <eric.spenner@hotmail.com>
2016-05-10 01:48:36 +0200

    walls disappear when between the player and the camera

---
+++ choramais-main-5b08fb329e49/choramais-main-5b08fb329e49/Assets/IVAR/Scripts/main/arrowManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+//using System.Collections;
+
+public class arrowManager : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        /*if (this.name != "arrow")
+            this.name = "arrow";*/
+            
+        Destroy(this.gameObject, 2.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag != "Player")
+            Destroy(this.gameObject);
+    }
+}
---
+++ choramais-main-5b08fb329e49/choramais-main-5b08fb329e49/Assets/IVAR/Scripts/main/weaponManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+//using System.Collections;
+
+public class weaponManager : MonoBehaviour
+{
+    public int damage = 50;
+    public int attackType = 0;
+    
+    // Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        /*if(Input.GetMouseButtonDown(0) && attackType == 0)
+            attackType = 1;
+        if(Input.GetMouseButtonDown(1) && attackType == 0)
+            attackType = 2;*/
+	}
+}
